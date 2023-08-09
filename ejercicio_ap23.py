3949caa7f69ac5299fc35182a28e32c09450373a
unknown <marypazch1@gmail.com>
2023-08-09 10:39:21 -0300

    agregue el ejercicio 4

--- ejercicio_ap23.py
+++ ejercicio_ap23.py
@@ -10,3 +10,16 @@ else:
     
 # bucles y estructuras de repeticion  while for
 
+"""Solicitar al usuario que ingrese dos números y mostrar
+ cuál de los dos es menor. Considerar el caso en que ambos números son iguales.
+ """
+ 
+number_1 = int(input("ingrese un numero"))
+number_2 = int(input("ingrese otro numero"))
+
+if number_1 < number_2:
+    print("este numero es el menor", number_1)
+elif number_2 < number_1:
+    print("este numero es el menor" ,number_2)
+else:
+    print("son iguales")
