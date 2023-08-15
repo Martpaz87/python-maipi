f80b62d5d9e3e885aa60210305e29ad2813fbf52
unknown <marypazch1@gmail.com>
2023-08-15 11:31:46 -0300

    creacion de priemr clase y objeto

--- poo.py
+++ poo.py
@@ -1,4 +1,18 @@
 #programacion orientada a objetos en python. clases : class. Una clase es un elemento de la programación orientada a objetos que actúa como una plantilla y va a definir las características y comportamientos de una entidad
 
 #fundamentos de la POO. Curso AP2023
+#crear una clase con python.
+"""
+Podemos crear una clase y no especificar nada dentro de ella. Para hacerlo más adelante. 
+La dejamos con pass y el programa compilará sin errores.
+En cambio, si omites el pass, te dará un error
+"""
+class FemaleSim:
+    experiencia = 1
+    resistencia = 100 #le agrego atributos.
+    aprendizaje = 1
+
+#objeto
+fsim_1 = FemaleSim()
+# si esta clase tuviese metodos y atributos , el objeto los estaria adquiriendo.
 
