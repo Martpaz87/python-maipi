8e884ebccd4e8fb7144077b35d6e9d0f76f13dd3
unknown <marypazch1@gmail.com>
2023-08-16 11:21:28 -0300

    metodo init y self

--- poo.py
+++ poo.py
@@ -27,3 +27,44 @@ fsim_1.hp = 0  #se le asigna un nuevo valor al atributo hp que solo afecta al ob
 
 if fsim_1.hp == 0:
     fsim_1.game_over()
+
+#metodo constructor __init__
+"""  sirve para inicializar valores de ciertas variables, es decir, 
+los de los atributos. por lo que de este modo,
+ siempre tendrás una serie de valores asignados por defecto al instanciar un objeto. se puede aprovechar para escribir menos codigo """
+
+class Sim:
+    experiencia = 1
+    resistencia = 10 #le agrego atributos.
+    capacidad_aprendizaje = 1
+    hp = 10 #puntos de vida
+    carisma = 10
+    habilidad_cocina = 10
+
+
+
+    def game_over(self):           #metodo:sería ejecutado de forma parecida a la de una función, ya que los métodos son en realidad funciones que pertenecen a una clase, nada más.
+        print("Game Over")
+
+#objeto 1
+female_sim = Sim()
+female_sim.habilidad_cocina = 2
+#objeto 2
+they_sim = Sim()
+they_sim.capacidad_aprendizaje = 9# se le asigna un valos diferente al de la clase. 
+they_sim.carisma = 8
+
+# self : self de Python hace referencia al nombre del objeto en el que se encuentra escrito
+class Sim:
+     def __init__(self, experiencia, resistencia, capacidad_aprendizaje, hp, carisma, habilidad_cocina):
+        self.experiencia = experiencia
+        self.resistencia = resistencia
+        self.capacidad_aprendizaje = capacidad_aprendizaje
+        self.hp = hp
+        self.carisma = carisma
+        
+#con el método __init__ es que vamos a evitar escribir cada vez las lineas de asignación de nuevos valores.
+#Si los escribes para un objeto, tampoco es tanto, ¿Y si son quinientos objetos con cincuenta atributos? Gracias al __init__evitarás este problema
+
+
+
