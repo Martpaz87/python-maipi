74f7022074067773a1562e98f41eaca618d1aac1
unknown <marypazch1@gmail.com>
2023-09-12 11:26:58 -0300

    ejercicio funciones

---
+++ ejercicios_2.py
@@ -0,0 +1,16 @@
+# Escribir una función que tome un carácter y devuelva True si es una vocal, de lo contrario devuelve False.
+
+def Vocales(x):
+
+
+    if x == "a" or x == "e" or x == "i" or x == "o" or x == "u":
+        return True
+       
+    elif x == "A" or x == "E" or x == "I" or x == "O" or x == "U":
+        return True
+    else:
+        return False
+
+        
+
+   
